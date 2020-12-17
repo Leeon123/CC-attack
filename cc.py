8f31b1712b6de8285b956d8d89fdc8d828eab827
ndbiaw <59746573+ndbiaw@users.noreply.github.com>
2020-12-17 10:34:43 +0700

    TabError
    inconsistent use of tabs and spaces in indentation

--- cc.py
+++ cc.py
@@ -613,17 +613,17 @@ def downloadsocks(choice):
 		f = open("socks5.txt",'wb')
 		try:
 			r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&simplified=true")
-	        f.write(r.content)
+			f.write(r.content)
 		except:
 			pass
 		try:
 			r = requests.get("https://www.proxy-list.download/api/v1/get?type=socks5")
-	        f.write(r.content)
+			f.write(r.content)
 		except:
 			pass
 		try:
 			r = requests.get("https://www.proxyscan.io/download?type=socks5")
-	        f.write(r.content)
+			f.write(r.content)
 		except:
 			pass
 		try:
