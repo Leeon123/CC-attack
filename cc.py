9fa622db5bd2b71b494bb8d77990ac257b36d8fd
Leeon123 <leeon123ex@gmail.com>
2020-12-19 00:17:13 +0800

    Update cc.py

--- cc.py
+++ cc.py
@@ -612,27 +612,27 @@ def downloadsocks(choice):
 	if choice == "5":
 		f = open("socks5.txt",'wb')
 		try:
-			r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&simplified=true")
+			r = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all&simplified=true",timeout=5)
 			f.write(r.content)
 		except:
 			pass
 		try:
-			r = requests.get("https://www.proxy-list.download/api/v1/get?type=socks5")
+			r = requests.get("https://www.proxy-list.download/api/v1/get?type=socks5",timeout=5)
 			f.write(r.content)
 		except:
 			pass
 		try:
-			r = requests.get("https://www.proxyscan.io/download?type=socks5")
+			r = requests.get("https://www.proxyscan.io/download?type=socks5",timeout=5)
 			f.write(r.content)
 		except:
 			pass
 		try:
-			r = requests.get("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt")
+			r = requests.get("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",timeout=5)
 			f.write(r.content)
 		except:
 			pass
 		try:
-			r = requests.get("https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt")
+			r = requests.get("https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",timeout=5)
 			f.write(r.content)
 			f.close()
 		except:
